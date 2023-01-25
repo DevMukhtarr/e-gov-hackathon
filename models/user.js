e71f7285930791577386a06f4cb7aadeb4fafb29
Your Name <you@example.com>
2023-01-25 19:28:13 +0100

    added more models

--- models/user.js
+++ models/user.js
@@ -2,12 +2,25 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const user = new Schema({
+    first_name: {type: String, default:null},
+    last_name: {type: String, default:null},
+    middle_name: {type: String, default:null},
     email: {type: String, default:null},
+    state: {type: String, default:null},
+    password: {type: String, default:null},
+    date_of_birth: {type: String, default:null},
+    date_created: {type: String, default:null},
+    date_modified: {type: String, default:null},
+    gender: {type: String, default:null},
+    picture: {type: String, default:null},
+    local_government_area: {type: String, default:null},
+    picture: {type: String, default:null},
     role: {
         type: String,
         required: true,
-        enum: ['admins','inec_official', 'agent', 'presiding_officer']
+        enum: ['admin','agent']
     },
+    is_active: {type: Boolean, default:false},
 })
 
 export default mongoose.model("User", user)
