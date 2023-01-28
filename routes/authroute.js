27a81bfc7acbde35559e10e48dea50ca6ba25433
Your Name <you@example.com>
2023-01-28 16:16:58 +0100

    changed AuthController.js to authController.js

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {signup, signin} from '../controllers/AuthController.js';
+import {signup, signin} from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/signup', signup);
